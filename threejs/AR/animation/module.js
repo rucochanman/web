a22902d6672ebdd516a735d44a82e04e1bb9f20d
rucochanman <49580224+rucochanman@users.noreply.github.com>
2020-12-23 23:40:24 +0900

    Create module.js

---
+++ threejs/AR/animation/module.js
@@ -0,0 +1,119 @@
+///////////////////////////////////////////////
+//    　　　     　   defs                   //
+//////////////////////////////////////////////
+
+const limbSeg = 5;
+const limbEdge = 12;
+const PI = Math.PI;
+const center2D = new THREE.Vector2();
+
+////////////////////////////////////////////////
+//    　　　　　　　 Math関数                  //
+///////////////////////////////////////////////
+
+function cos( r ){
+    return Math.cos( r );
+};
+function sin( r ){
+    return Math.sin( r );
+};
+function pow( base,exp ){
+    return Math.pow( base,exp );
+};
+
+////////////////////////////////////////////////
+//    　　　　　　　 pipe作成                  //
+///////////////////////////////////////////////
+
+function makePipe( obj ){
+    //make bone
+    const curve = new THREE.QuadraticBezierCurve( center2D, obj.cp, obj.ep );
+    const bone = curve.getPoints( obj.seg );
+    let zpos = bone[0];
+    //set points
+    const pt = [];
+    for( let i=0; i<( obj.seg+1 ); i++ ){
+        //calc angle
+        const diff = new THREE.Vector2().subVectors( bone[i], zpos );
+        const angle = Math.atan2( diff.y, diff.x );
+        //calc coords
+        pt[i] = [];
+        for( let j=0; j<obj.edge; j++ ){
+            const theta = j * 2 * PI / obj.edge;
+            const w = obj.thick[i] * cos( theta );
+            const h = obj.width[i] * sin( theta );
+            const v = new THREE.Vector2( 0, h );
+            v.add( bone[i] );
+            v.rotateAround( bone[i], angle );
+            pt[i][j] = [v.x, v.y, w];
+        }
+        zpos = bone[i];
+    }
+    return pt;
+}
+
+////////////////////////////////////////////////
+//    　　　　　  vertexの作成                 //
+///////////////////////////////////////////////
+
+function setVertices( seg, edge, pt ){
+    const vert = [];
+    for( let i=0; i<seg; i++ ){
+        vert[i] = [];
+        for( let j=0; j<edge; j++ ){
+            vert[i][j] = [];
+            vert[i][j][0] = pt[i][j];
+            vert[i][j][1] = pt[i][( j+1 ) % edge];
+            vert[i][j][2] = pt[i+1][( j+1 ) % edge];
+            vert[i][j][3] = pt[i+1][j];
+        }
+    }
+    return new Float32Array( vert.flat(3) );
+}
+
+////////////////////////////////////////////////
+//    　　　 　　  indexの作成                 //
+///////////////////////////////////////////////
+
+function setIndices( seg, edge ){
+    const num_rect = seg * edge;
+    const order = [0,3,2,2,1,0];
+    const index = [];
+    for( let i=0; i<num_rect; i++ ){
+        for( let j=0; j<order.length; j++ ){
+            index.push( order[j]+(4*i) );
+        }
+    }
+    return new Uint16Array( index );
+}
+
+////////////////////////////////////////////////
+//    　　   BufferGeometryの作成             //
+///////////////////////////////////////////////
+
+function makeGeometry( obj, pt ){
+    const vertices = setVertices( obj.seg, obj.edge, pt );
+    const indices = setIndices( obj.seg, obj.edge );
+    const geometry = new THREE.BufferGeometry();
+    geometry.addAttribute('position', new THREE.BufferAttribute( vertices, 3 ));
+    geometry.setIndex(new THREE.BufferAttribute( indices, 1 ));
+    const merg = new THREE.Geometry().fromBufferGeometry( geometry );
+    merg.mergeVertices();
+    merg.computeVertexNormals();
+    return merg;
+}
+
+////////////////////////////////////////////////
+//    　　   geometryのアップデート            //
+///////////////////////////////////////////////
+
+function updateGeometry( obj, pt, geometry ){
+    const vertices = setVertices( obj.seg, obj.edge, pt );
+    const newGeo = new THREE.BufferGeometry();
+    newGeo.addAttribute('position', new THREE.BufferAttribute( vertices, 3 ));
+    const merg = new THREE.Geometry().fromBufferGeometry( newGeo );
+    merg.mergeVertices();
+    geometry.vertices = merg.vertices;
+    geometry.elementsNeedUpdate = true;
+    geometry.computeVertexNormals();
+}
