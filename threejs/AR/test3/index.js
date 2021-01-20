51075d95f237e443a0dbde250c3cc4f8bb08a60d
rucochanman <49580224+rucochanman@users.noreply.github.com>
2021-01-20 16:20:25 +0900

    Update index.js

--- threejs/AR/test3/index.js
+++ threejs/AR/test3/index.js
@@ -172,7 +172,7 @@ function init() {
         armG.add( upperArmMesh );
         armG.add( lowerArmG );
         //add mesh to scene
-        armG.position.y = 0.1;
+        armG.position.y = 1;
         armG.scale.set( 0.05, 0.05, 0.05 );
         marker1.add( armG );
     }
