667be5029a6d8606323ff7fab72107f962f311be
rucochanman <49580224+rucochanman@users.noreply.github.com>
2021-01-20 14:50:07 +0900

    Update index.js

--- threejs/AR/test3/index.js
+++ threejs/AR/test3/index.js
@@ -1,38 +1,37 @@
-window.addEventListener("DOMContentLoaded", init);
-
+window.addEventListener( "DOMContentLoaded", init );
 function init() {
-    //レンダラーの設定
+
+    ///////////////////////////////////////////////
+    //    　　　　　　 画面設定                   //
+    //////////////////////////////////////////////
+
+    //レンダラーの作成
     const renderer = new THREE.WebGLRenderer({
         antialias: true,
         alpha: true
     });
-    renderer.setClearColor(new THREE.Color(), 0);
-    renderer.setSize(640, 480);
+    renderer.setClearColor( new THREE.Color(), 0 );
+    renderer.setSize( 640, 480 );
     renderer.domElement.style.position = 'absolute';
     renderer.domElement.style.top = '0px';
     renderer.domElement.style.left = '0px';
-    document.body.appendChild(renderer.domElement);
-　　　
+    document.body.appendChild( renderer.domElement );
     //画面設定
     const scene = new THREE.Scene();
     scene.visible = false;
     const camera = new THREE.Camera();
     scene.add(camera);
-    const light = new THREE.AmbientLight(0xFFFFFF, 1.0);
+    const light = new THREE.AmbientLight( 0xFFFFFF, 1.0 );
     scene.add(light);
-
     //画面リサイズの設定
-    window.addEventListener('resize', () => {
-        onResize();
-    });
+    window.addEventListener('resize', () => { onResize() });
     function onResize() {
         arToolkitSource.onResizeElement();
-        arToolkitSource.copyElementSizeTo(renderer.domElement);
-        if (arToolkitContext.arController !== null) {
-            arToolkitSource.copyElementSizeTo(arToolkitContext.arController.canvas);
+        arToolkitSource.copyElementSizeTo( renderer.domElement );
+        if ( arToolkitContext.arController !== null ) {
+            arToolkitSource.copyElementSizeTo( arToolkitContext.arController.canvas );
         }
     };
-
     //AR周りの設定
     const arToolkitSource = new THREEx.ArToolkitSource({
         sourceType: 'webcam'
@@ -40,41 +39,81 @@ function init() {
     arToolkitSource.init(() => {
         setTimeout(() => {
             onResize();
-        }, 2000);
+        }, 2000 );
     });
     const arToolkitContext = new THREEx.ArToolkitContext({
         cameraParametersUrl: 'data/camera_para.dat',
         detectionMode: 'mono'
     });
     arToolkitContext.init(() => {
-        camera.projectionMatrix.copy(arToolkitContext.getProjectionMatrix());
+        camera.projectionMatrix.copy( arToolkitContext.getProjectionMatrix() );
     });
 
-    //マーカー設定
+    ///////////////////////////////////////////////
+    //    　　       マーカーの設定               //
+    //////////////////////////////////////////////
+
     const marker1 = new THREE.Group();
-    scene.add(marker1);
-    const arMarkerControls = new THREEx.ArMarkerControls(arToolkitContext, marker1, {
+    scene.add( marker1 );
+    const arMarkerControls = new THREEx.ArMarkerControls( arToolkitContext, marker1, {
         type: 'pattern',
         patternUrl: 'data/pattern-sneak.patt',
     });
 
-    //モデル読み込み
+    ///////////////////////////////////////////////
+    //    　　      モデルの読み込み              //
+    //////////////////////////////////////////////
+
+    /*
     let apple;
     const gltfloader = new THREE.GLTFLoader();
-    gltfloader.load('./data/apple.glb',function(gltf){
+    gltfloader.load( './data/apple.glb',function( gltf ){
         apple = gltf.scene;
-        apple.scale.set(0.5, 0.5, 0.5);
-        marker1.add(apple);
+        apple.scale.set( 0.5, 0.5, 0.5 );
+        marker1.add( apple );
+    });
+    */
+
+    const armMat = new THREE.MeshNormalMaterial({
+        side:THREE.DoubleSide,
     });
 
-    //レンダリング
-    requestAnimationFrame(function animate(){
-        requestAnimationFrame(animate);
-        if (arToolkitSource.ready) {
-            arToolkitContext.update(arToolkitSource.domElement);
+    const upperArmLength = 20;
+    const lowerArmLength = 20;
+    const upperArmThick = 5;
+    const fingerLength = 2;
+    const fingerThick = 1;
+
+    const upperArmObj = new Limbs();
+    const jointArmObj = new Limbs();
+    const lowerArmObj = new Limbs();
+    const fingerObj = new Limbs();
+
+    let upperArmGeo;
+    let jointArmGeo;
+    let lowerArmGeo;
+
+    const armG = new THREE.Group();
+    const lowerArmG = new THREE.Group();
+    const handG = new THREE.Group();
+
+    const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+    const cube = new THREE.Mesh( geometry, armMat );
+    marker1.add( apple );
+
+    
+
+    ///////////////////////////////////////////////
+    //    　　　　  　レンダリング開始             //
+    //////////////////////////////////////////////
+
+    requestAnimationFrame( function animate(){
+        requestAnimationFrame( animate );
+        if ( arToolkitSource.ready ) {
+            arToolkitContext.update( arToolkitSource.domElement );
             scene.visible = camera.visible;
         }
-        apple.rotation.z += 0.01;
-        renderer.render(scene, camera);
+        //apple.rotation.z += 0.01;
+        renderer.render( scene, camera );
     });
 }
