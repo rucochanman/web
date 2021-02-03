38e30c6b018b1a6e910b9c086ef5dd2243687081
rucochanman <49580224+rucochanman@users.noreply.github.com>
2021-02-03 15:59:17 +0900

    Update module.js

--- threejs/AR/test3/module.js
+++ threejs/AR/test3/module.js
@@ -10,32 +10,24 @@ const center2D = new THREE.Vector2();
 let lastAngle = 0;
 let lastPos = new THREE.Vector2();
 
+
 ////////////////////////////////////////////////
-//    　　　　　　　 Math関数                  //
+//    　　　       　 reset                   //
 ///////////////////////////////////////////////
 
-function cos( r ){
-    return Math.cos( r );
-};
-function sin( r ){
-    return Math.sin( r );
-};
-function pow( base,exp ){
-    return Math.pow( base,exp );
-};
-function abs( r ){
-    return Math.abs( r );
-};
-
+function lastValClear(){
+    lastAngle = 0;
+    lastPos = new THREE.Vector2();
+}
 
 ////////////////////////////////////////////////
 //    　　　 　ベジエポイント取得               //
 ///////////////////////////////////////////////
 
 function getBezierPt( len, bend ){
-    const ep_x = len * cos( bend );
-    const ep_y = len * sin( bend );
-    const cp_x = abs( ep_y / 2 );
+    const ep_x = len * Math.cos( bend );
+    const ep_y = len * Math.sin( bend );
+    const cp_x = Math.abs( ep_y / 2 );
     const cp_y = Math.max( ep_y / 2, 0 );
     const ep = new THREE.Vector2( ep_x, ep_y );
     const cp = new THREE.Vector2( cp_x, cp_y );
@@ -43,7 +35,7 @@ function getBezierPt( len, bend ){
 }
 
 function getBezierPt2( bend, len, thick ){
-    const joint_len = thick * abs( bend );
+    const joint_len = thick * Math.abs( bend );
     const v = new THREE.Vector2( len - joint_len, 0 );
     const ep = v.rotateAround( center2D, lastAngle );
     const cp = ep.clone();
@@ -69,8 +61,8 @@ function makePipePt( obj ){
         pt[i] = [];
         for( let j=0; j<obj.edge; j++ ){
             const theta = j * 2 * PI / obj.edge;
-            const w = obj.thick[i] * cos( theta );
-            const h = obj.width[i] * sin( theta );
+            const w = obj.thick[i] * Math.cos( theta );
+            const h = obj.width[i] * Math.sin( theta );
             const v = new THREE.Vector2( 0, h );
             v.add( bone[i] );
             v.rotateAround( bone[i], angle );
@@ -84,7 +76,6 @@ function makePipePt( obj ){
     return pt;
 }
 
-
 function makeJointPt( obj, bend ){
     //make bone center
     const radius = obj.thick[0];
@@ -93,14 +84,14 @@ function makeJointPt( obj, bend ){
     //set points
     const bone = new THREE.Vector2();
     const pt = [];
-    for( let i=0; i<( obj.seg ); i++ ){
+    for( let i=0; i<obj.seg; i++ ){
         pt[i] = [];
         let angle = i==0 ? 0 : bend / ( obj.seg-1 );
         bone.rotateAround( origin, angle );
         for( let j=0; j<obj.edge; j++ ){
-            const theta = j * 2 * PI / obj.edge;
-            const w = radius * cos( theta );
-            const h = radius * sin( theta );
+            const theta = j * 2 * Math.PI / obj.edge;
+            const w = radius * Math.cos( theta );
+            const h = radius * Math.sin( theta );
             const v = new THREE.Vector2( 0, h );
             v.add( bone );
             v.rotateAround( bone, i * angle + lastAngle );
@@ -115,6 +106,42 @@ function makeJointPt( obj, bend ){
 
 
 ////////////////////////////////////////////////
+//    　　　 　　  uvmapの作成                 //
+///////////////////////////////////////////////
+
+function makeUvmap( obj ){
+    const uvmap = [];
+    for( let i=0; i<( obj.seg+1 ); i++ ){
+        uvmap[i] = [];
+        const y = i / obj.seg;
+        for( let j=0; j<( obj.edge+1 ); j++ ){
+              const x = j / obj.edge;
+              uvmap[i][j] = [x, y];
+        }
+    }
+    return uvmap;
+}
+
+////////////////////////////////////////////////
+//    　　　　　     uvの作成                  //
+///////////////////////////////////////////////
+
+function setUvs( seg, edge, pt ){
+    const vert = [];
+    for( let i=0; i<seg; i++ ){
+        vert[i] = [];
+        for( let j=0; j<edge; j++ ){
+            vert[i][j] = [];
+            vert[i][j][0] = pt[i][j];
+            vert[i][j][1] = pt[i][j+1];
+            vert[i][j][2] = pt[i+1][j+1];
+            vert[i][j][3] = pt[i+1][j];
+        }
+    }
+    return new Float32Array( vert.flat(3) );
+}
+
+////////////////////////////////////////////////
 //    　　　　　  vertexの作成                 //
 ///////////////////////////////////////////////
 
@@ -125,8 +152,8 @@ function setVertices( seg, edge, pt ){
         for( let j=0; j<edge; j++ ){
             vert[i][j] = [];
             vert[i][j][0] = pt[i][j];
-            vert[i][j][1] = pt[i][( j+1 ) % edge];
-            vert[i][j][2] = pt[i+1][( j+1 ) % edge];
+            vert[i][j][1] = pt[i][(j+1) % edge];
+            vert[i][j][2] = pt[i+1][(j+1) % edge];
             vert[i][j][3] = pt[i+1][j];
         }
     }
@@ -153,11 +180,13 @@ function setIndices( seg, edge ){
 //    　　   BufferGeometryの作成             //
 ///////////////////////////////////////////////
 
-function makeGeometry( obj, pt ){
+function makeGeometry( obj, pt, uv ){
     const vertices = setVertices( obj.seg, obj.edge, pt );
+    const uvs = setUvs( obj.seg, obj.edge, uv );
     const indices = setIndices( obj.seg, obj.edge );
     const geometry = new THREE.BufferGeometry();
     geometry.setAttribute('position', new THREE.BufferAttribute( vertices, 3 ));
+    geometry.setAttribute('uv', new THREE.BufferAttribute( uvs, 2 ));
     geometry.setIndex(new THREE.BufferAttribute( indices, 1 ));
     const merg = new THREE.Geometry().fromBufferGeometry( geometry );
     merg.mergeVertices();
