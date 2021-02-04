8bcd79380eaf28f9d92689639af9ef8040f0dcec
rucochanman <49580224+rucochanman@users.noreply.github.com>
2021-02-04 18:09:33 +0900

    Update module.js

--- Baddies/module.js
+++ Baddies/module.js
@@ -9,10 +9,11 @@ const PI = Math.PI;
 const center2D = new THREE.Vector2();
 let lastAngle = 0;
 let lastPos = new THREE.Vector2();
+const texLoader = new THREE.TextureLoader();
 
-const upperArmLength = 12;
-const lowerArmLength = 18;
-const upperArmThick = 5;
+const upperArmLength = 6;
+const lowerArmLength = 9;
+const upperArmThick = 2.5;
 
 const upperArmObj = new Limbs();
 const jointArmObj = new Limbs();
