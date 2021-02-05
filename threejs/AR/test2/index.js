854177be7f3af4bc6e26c3eae5bcef9ba71373cf
rucochanman <49580224+rucochanman@users.noreply.github.com>
2021-02-05 21:56:29 +0900

    Update index.js

--- threejs/AR/test2/index.js
+++ threejs/AR/test2/index.js
@@ -1,80 +1,108 @@
-window.addEventListener("DOMContentLoaded", init);
-
+//ページの読み込みを待つ
+window.addEventListener('load', init);
 function init() {
-    //レンダラーの設定
+
+    ///////////////////////////////////////////////
+    //    　　　　　　 画面設定                   //
+    //////////////////////////////////////////////
+
+    //画面サイズを指定
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    //レンダラーを作成
     const renderer = new THREE.WebGLRenderer({
-        antialias: true,
-        alpha: true
+        canvas: document.querySelector( '#myCanvas' )
     });
-    renderer.setClearColor(new THREE.Color(), 0);
-    renderer.setSize(640, 480);
-    renderer.domElement.style.position = 'absolute';
-    renderer.domElement.style.top = '0px';
-    renderer.domElement.style.left = '0px';
-    document.body.appendChild(renderer.domElement);
-　　　
-    //画面設定
+    renderer.setClearColor( new THREE.Color( 'grey' ) );
+    document.body.appendChild( renderer.domElement );
+    renderer.setSize( width, height );
+    //シーンを作成
     const scene = new THREE.Scene();
-    scene.visible = false;
-    const camera = new THREE.Camera();
-    scene.add(camera);
-    const light = new THREE.AmbientLight(0xFFFFFF, 1.0);
-    scene.add(light);
+    //カメラを作成
+    const camera = new THREE.PerspectiveCamera( 45, width / height );
+    camera.position.set( 20, 0, 80 );
+    camera.lookAt(new THREE.Vector3( 20, 0, 0 ));
+    //ライトを設置
+    const envlight = new THREE.AmbientLight( 0xffffff, 0.8 );
+    scene.add(envlight);
+    const light1 = new THREE.DirectionalLight( 0xffffff, 0.8 );
+    light1.position.set(0, 0, 10);
+    scene.add(light1);
+    //const light2 = new THREE.DirectionalLight( 0xffffff, 0.25 );
+    //light2.position.set(0, 0, 10);
+    //scene.add(light2);
 
-    //画面リサイズの設定
-    window.addEventListener('resize', () => {
-        onResize();
-    });
-    function onResize() {
-        arToolkitSource.onResizeElement();
-        arToolkitSource.copyElementSizeTo(renderer.domElement);
-        if (arToolkitContext.arController !== null) {
-            arToolkitSource.copyElementSizeTo(arToolkitContext.arController.canvas);
-        }
-    };
-      
-    //AR周りの設定
-    const arToolkitSource = new THREEx.ArToolkitSource({
-        sourceType: 'webcam'
-    });
-    arToolkitSource.init(() => {
-        setTimeout(() => {
-            onResize();
-        }, 2000);
-    });
-    const arToolkitContext = new THREEx.ArToolkitContext({
-        cameraParametersUrl: 'data/camera_para.dat',
-        detectionMode: 'mono'
-    });
-    arToolkitContext.init(() => {
-        camera.projectionMatrix.copy(arToolkitContext.getProjectionMatrix());
-    });
-    
-    //マーカー設定  
-    const marker1 = new THREE.Group();
-    scene.add(marker1);
-    const arMarkerControls = new THREEx.ArMarkerControls(arToolkitContext, marker1, {
-        type: 'pattern',
-        patternUrl: 'data/pattern-sneak.patt',
-    });
-    
-    //モデル読み込み
-    let apple;
-    const gltfloader = new THREE.GLTFLoader();
-    gltfloader.load('./data/apple.glb',function(gltf){
-        apple = gltf.scene;
-        apple.scale.set(0.5, 0.5, 0.5);
-        marker1.add(apple);
-    });
+    ///////////////////////////////////////////////
+    //    　　　　  　 model作成                 //
+    //////////////////////////////////////////////
 
-    //レンダリング
-    requestAnimationFrame(function animate(){
-        requestAnimationFrame(animate);
-        if (arToolkitSource.ready) {
-            arToolkitContext.update(arToolkitSource.domElement);
-            scene.visible = camera.visible;
-        }
-        apple.rotation.z += 0.01;
+
+    makeModel( scene );
+
+    ///////////////////////////////////////////////
+    //    　　　　  　animation設定               //
+    //////////////////////////////////////////////
+
+    // POSITION
+    const upperArmMove = new THREE.Object3D();
+    const dur = [ 0, 2, 4 ];
+    const posVal1 = [ -0.2, 1, -0.2 ];
+    const posVal2 = [ 0, 1.5, 0 ];
+    const rotVal1 = [ 0, 0, 0 ];
+    const rotVal2 = [ 0, -PI/2, 0 ];
+
+    const upperArmPos = [];
+    const upperArmRot = [];
+    for( let i=0; i<dur.length; i++ ){
+        upperArmPos.push( posVal1[i] );
+        upperArmPos.push( posVal2[i] );
+        upperArmPos.push( 0 );
+        upperArmRot.push( rotVal1[i] );
+        upperArmRot.push( rotVal2[i] );
+        upperArmRot.push( 0 );
+    }
+
+    const uppperArmKF1 = new THREE.NumberKeyframeTrack( '.position', dur, upperArmPos );
+    const uppperArmKF2 = new THREE.NumberKeyframeTrack( '.scale', dur, upperArmRot );
+    const clip = new THREE.AnimationClip( 'Action', 4, [ uppperArmKF1, uppperArmKF2 ] );
+    const mixer = new THREE.AnimationMixer( upperArmMove );
+    const clipAction = mixer.clipAction( clip );
+    clipAction.play();
+
+
+    function roadUpdate(){
+        const zpos = road.geometry.faceVertexUvs[0][0][2].x - 0.01;
+        const pos1 = zpos < 0.5 ? 1.0 : zpos;
+        const pos0 = pos1 - 0.5;
+        road.geometry.faceVertexUvs[0][0][0].x = pos0;
+        road.geometry.faceVertexUvs[0][0][1].x = pos0;
+        road.geometry.faceVertexUvs[0][0][2].x = pos1;
+        road.geometry.faceVertexUvs[0][1][0].x = pos0;
+        road.geometry.faceVertexUvs[0][1][1].x = pos1;
+        road.geometry.faceVertexUvs[0][1][2].x = pos1;
+        road.geometry.uvsNeedUpdate = true;
+    }
+
+    ///////////////////////////////////////////////
+    //    　　　　  　レンダリング開始             //
+    //////////////////////////////////////////////
+
+    const clock = new THREE.Clock();
+    render();
+
+    function update(){
+        mixer.update( clock.getDelta() );
+        let angle1 = upperArmMove.position.x;
+        let angle2 = upperArmMove.position.y;
+        let rot1 = upperArmMove.scale.x;
+        let rot2 = upperArmMove.scale.y;
+        armUpdate( crowley, angle1, angle2, rot1, rot2 );
+
+    }
+
+    function render(){
+        update();
+        requestAnimationFrame(render);
         renderer.render(scene, camera);
-    });
+    }
 }
