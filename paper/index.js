14b862621ec0c832a688196d5c2c17f1ffd40450
rucochanman <49580224+rucochanman@users.noreply.github.com>
2021-06-30 15:37:09 +0900

    Update index.js

--- paper/index.js
+++ paper/index.js
@@ -131,6 +131,27 @@ function init() {
         markerArray[1].add( aziraphaleBody2 );
     });
 
+    let ice1;
+    gltfloader.load( './data/model/ice1.glb',function( gltf ){
+        ice1 = gltf.scene;
+        ice1.scale.set( 0.25, 0.25, 0.25 );
+        ice1.position.x = -0.5;
+        ice1.position.z = 0.28;
+        ice1.position.y = 0.47;
+        ice1.rotation.z = PI/4;
+        markerArray[3].add( ice1 );
+    });
+
+    let ice2;
+    gltfloader.load( './data/model/ice2.glb',function( gltf ){
+        ice2 = gltf.scene;
+        ice2.scale.set( 0.25, 0.25, 0.25 );
+        ice2.position.x = 0.5;
+        ice2.position.z = 0.28;
+        ice2.position.y = 0.47;
+        ice2.rotation.z = -PI/4;
+        markerArray[3].add( ice2 );
+    });
 
     let apple;
     gltfloader.load( './data/model/apple.glb',function( gltf ){
@@ -139,7 +160,75 @@ function init() {
         markerArray[0].add( apple );
     });
 
+    let bentley;
+    gltfloader.load( './data/model/bentley.glb',function( gltf ){
+        bentley = gltf.scene;
+        bentley.scale.set( 0.65, 0.65, 0.65 );
+        bentley.position.y = 0.5;
+        bentley.position.z = -0.25;
+        bentley.rotation.y = -PI/2;
+        markerArray[2].add( bentley );
+    });
 
+    let bench;
+    gltfloader.load( './data/model/bench.glb',function( gltf ){
+        bench = gltf.scene;
+        bench.scale.set( 0.7, 0.7, 0.7 );
+        bench.position.z = -0.25;
+        markerArray[3].add( bench );
+    });
+
+    const grassTex = texLoader.load( './data/tex/grass.png' );
+    const grass = new THREE.Mesh(
+        new THREE.PlaneGeometry( 1.65, 1.6 ),
+        new THREE.MeshBasicMaterial( {map:grassTex} )
+    );
+    grass.rotation.x = -PI/2;
+    markerArray[3].add( grass );
+
+    const roadTex = texLoader.load( './data/tex/road.png' );
+    const road = new THREE.Mesh(
+        new THREE.PlaneGeometry( 1.5, 1.5 ),
+        new THREE.MeshBasicMaterial( {map:roadTex} )
+    );
+    const roadUv1 = [
+        new THREE.Vector2(0, 1),
+        new THREE.Vector2(0, 0),
+        new THREE.Vector2(0.2, 1)
+    ];
+    const roadUv2 = [
+        new THREE.Vector2(0, 0),
+        new THREE.Vector2(0.2, 0),
+        new THREE.Vector2(0.2, 1.0)
+    ];
+    road.geometry.faceVertexUvs[0][0] = roadUv1;
+    road.geometry.faceVertexUvs[0][1] = roadUv2;
+    road.rotation.x = -PI/2;
+    markerArray[2].add( road );
+
+    //box
+    const planeSize = 1;
+    const planeGeo = new THREE.PlaneGeometry( planeSize, planeSize );
+    const planeMat = new THREE.MeshLambertMaterial( {
+      color: 0x550055,
+      opacity: 0.9,
+      transparent: true,
+      side: THREE.DoubleSide
+    } );
+    const plane = new THREE.Mesh( planeGeo, planeMat );
+    plane.rotation.x = PI/2;
+    plane.position.y = -planeSize/2;
+    markerArray[1].add( plane );
+    for( let i=0; i<4; i++ ){
+        const side = new THREE.Mesh( planeGeo, planeMat );
+        const rot = i * -PI/2;
+        const posx = [0, -planeSize/2, 0, planeSize/2];
+        const posz = [planeSize/2, 0, -planeSize/2, 0];
+        side.rotation.y = rot;
+        side.position.x = posx[i];
+        side.position.z = posz[i];
+        markerArray[1].add( side );
+    }
 
     makeModel( markerArray );
 
@@ -160,9 +249,19 @@ function init() {
     const clipAction = mixer.clipAction( clip );
     clipAction.play();
 
-
-    
-    
+    // road
+    function roadUpdate(){
+        const zpos = road.geometry.faceVertexUvs[0][0][2].x - 0.03;
+        const pos1 = zpos < 0.2 ? 1.0 : zpos;
+        const pos0 = pos1 - 0.2;
+        road.geometry.faceVertexUvs[0][0][0].x = pos0;
+        road.geometry.faceVertexUvs[0][0][1].x = pos0;
+        road.geometry.faceVertexUvs[0][0][2].x = pos1;
+        road.geometry.faceVertexUvs[0][1][0].x = pos0;
+        road.geometry.faceVertexUvs[0][1][1].x = pos1;
+        road.geometry.faceVertexUvs[0][1][2].x = pos1;
+        road.geometry.uvsNeedUpdate = true;
+    }
 
     ///////////////////////////////////////////////
     //    　　　　  　レンダリング開始             //
